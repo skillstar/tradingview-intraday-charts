bb9ba877acaa2c51e4dd51f1b101d0f4e31868c0
yangsheng8 <344182792@qq.com>
2024-10-20 23:46:28 +0800

    bug: api edge

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,11 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
+export const runtime = 'edge';
 
 type Data = {
   name: string;
 };
-export const runtime = 'edge';
 
 export default function handler(
   req: NextApiRequest,

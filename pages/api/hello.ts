b10ed7e45f1f652ab26c0fa782455ac499d9c7c0
yangsheng8 <344182792@qq.com>
2024-10-20 23:42:04 +0800

    bug: npx vercel build

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,9 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
+
 type Data = {
   name: string;
 };
+export const runtime = 'edge';
 
 export default function handler(
   req: NextApiRequest,
